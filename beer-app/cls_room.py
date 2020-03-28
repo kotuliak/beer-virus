d56e7bff82c239289f714e86ca3b0bb3da3acb4a
Luca Albinati <>
2020-03-28 18:47:52 +0100

    fix merge error

--- beer-app/cls_room.py
+++ beer-app/cls_room.py
@@ -1,16 +1,49 @@
-import random
 from cls_users import Users
 
 class Room:
     def __init__(self):
-        self.id = random.randrange(0, 1000)
-        self.users = list()
-        print("id is " + str(self.id))
+        self.users = dict()
+        self.user_locations = dict()
 
     def create_user(self, name):
-        self.users.append(Users(name))
+        self.users[name] = Users(name)
+        self.user_locations[name] = None
+    
+    def assign_location(self, name, location):
+        self.user_locations[name] = location
+        if not(None in self.user_locations.values()):
+            self.check_infection()
+    
+    def check_infection(self):
+        infected_locations = list()
+        for user in self.user_locations:
+            if self.users[user].get_state() == "Sick":
+                infected_locations.append(self.user_locations[user])
 
-    def get_user(self, name):
+        infected_locations = list(set(infected_locations))
+
+        for user in self.user_locations:
+            if self.user_locations[user] in infected_locations:
+                self.users[user].infect()
+
+    def check_game_status(self):
+        ind = True
         for user in self.users:
-            if user.name == name:
-                return user
+            if self.users[user].get_state() == "Healthy":
+                ind = False
+            self.user_locations[user] = None
+        if ind:
+            print("Game Over")
+
+r1 = Room()
+r1.create_user('Michal')
+r1.create_user('Matej')
+print(r1.user_locations)
+r1.assign_location('Michal', 'Bathroom')
+print(r1.user_locations)
+r1.users['Matej'].infect()
+r1.assign_location('Matej', "Bathroom")
+print(r1.user_locations)
+print(r1.users['Michal'].get_state())
+r1.check_game_status()
+print(r1.user_locations)
