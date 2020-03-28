2f8ba7c93defd8785e3827070772d58b3f92c3a3
Luca Albinati <>
2020-03-28 19:05:15 +0100

    fix id

--- beer-app/cls_room.py
+++ beer-app/cls_room.py
@@ -1,7 +1,9 @@
+import random
 from cls_users import Users
 
 class Room:
     def __init__(self):
+        self.id = random.randrange(0, 1000)
         self.users = dict()
         self.user_locations = dict()
 
