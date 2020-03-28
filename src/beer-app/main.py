10e7ee4a46acf2b043835013e466def320e0d8f7
Martin Kotuliak <martin@Martins-MacBook-Pro.local>
2020-03-28 16:27:05 +0100

    adding users

--- src/beer-app/main.py
+++ src/beer-app/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +11,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/add", methods=["POST"])
+def add():
+    a = request.form.get("new-name", 0, type=str)
+    users=[{"name":"a"},{"name":"a"},{"name":"a"}]
+    users.append({"name":a})
+    return render_template("list_users.html", users=users)
+
+@app.route("/room")
+def room():
+    users=[{"name":"a"},{"name":"a"},{"name":"a"}]
+    return render_template("room.html", users=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
