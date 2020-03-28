ef78f90cfc55d109fabce49d008c1fc167529c65
mkobak <56454683+mkobak@users.noreply.github.com>
2020-03-28 17:36:24 +0100

    location functionality of room

--- src/beer-app/cls_room.py
+++ src/beer-app/cls_room.py
@@ -1,13 +1,40 @@
-import numpy as np
 from cls_users import Users
 
 class Room:
     def __init__(self):
-        self.users = list()
+        self.users = dict()
+        self.user_locations = dict()
 
     def create_user(self, name):
-        self.users.append(Users(name))
+        self.users[name] = Users(name)
+        self.user_locations[name] = None
+    
+    def assign_location(self, name, location):
+        self.user_locations[name] = location
+        if not(None in self.user_locations.values()):
+            self.check_infection()
+    
+    def check_infection(self):
+        infected_locations = list()
+        for user in self.user_locations:
+            if self.users[user].get_state() == "Sick":
+                infected_locations.append(self.user_locations[user])
+
+        infected_locations = list(set(infected_locations))
+
+        for user in self.user_locations:
+            if self.user_locations[user] in infected_locations:
+                self.users[user].infect()
+
+    
 
 r1 = Room()
 r1.create_user('Michal')
-r1.users[0].check()
+r1.create_user('Matej')
+print(r1.user_locations)
+r1.assign_location('Michal', 'Bathroom')
+print(r1.user_locations)
+r1.users['Matej'].infect()
+r1.assign_location('Matej', "Bathroom")
+print(r1.user_locations)
+print(r1.users['Michal'].get_state())
