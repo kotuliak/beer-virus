d02ab1c4df09ea79de614d18b0873a5be40bb4ed
mkobak <56454683+mkobak@users.noreply.github.com>
2020-03-28 15:38:31 +0100

    Create cls_room.py
    Class room

---
+++ src/beer-app/cls_room.py
@@ -0,0 +1,13 @@
+import numpy as np
+from cls_users import Users
+
+class Room:
+    def __init__(self):
+        self.users = list()
+
+    def create_user(self, name):
+        self.users.append(Users(name))
+
+r1 = Room()
+r1.create_user('Michal')
+r1.users[0].check()
