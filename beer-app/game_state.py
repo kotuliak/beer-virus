ccc4276fdf9201a250d8491d699da1c99beb1538
Luca Albinati <>
2020-03-29 16:06:30 +0200

    fix some errors in room

--- beer-app/game_state.py
+++ beer-app/game_state.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 class GameState(Enum):
-    PLAYING = 0
-    GOOD_GUYS_WON = 1
-    BAD_GUYS_WON = 2
+    NOT_STARTED = 0
+    STARTED = 1
+    GOOD_GUYS_WON = 2
+    BAD_GUYS_WON = 3
